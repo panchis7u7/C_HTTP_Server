237d6542bd9a1d8ffaa0442a54c2e58454d9623b
Panchis7u7 <smadrigal.rod@gmail.com>
2020-08-05 23:15:22 -0500

    Implementacion de manejador de nombre de archivos para estandares MIME.

---
+++ Mime.c
@@ -0,0 +1,57 @@
+#include "Mime.h"
+#include <ctype.h>
+#include <string.h>
+
+#define TIPO_MIME_PREDETERMINADO "application/octet-stream"
+
+//Prototipo de funciones.
+char* String_minusculas(char*);
+
+//Convierte a minusculas toda una cadena de caracteres.
+char* String_minusculas(char* s){
+    char* p;
+    for (p = s; *p != '\0'; p++)
+    {
+        *p = tolower(*p);
+    }
+    return s;
+}
+
+//Regresa un tipo MIME para un nombre de archivo dado.
+char *mime_type_get(char *filename) {
+  char *ext = strrchr(filename, '.');
+
+  if (ext == NULL) {
+    return TIPO_MIME_PREDETERMINADO;
+  }
+  ext++;
+  String_minusculas(ext);
+  // Esto es O(n) y deberia de ser O(1).
+
+  if (strcmp(ext, "html") == 0 || strcmp(ext, "htm") == 0) {
+    return "text/html";
+  }
+  if (strcmp(ext, "jpeg") == 0 || strcmp(ext, "jpg") == 0) {
+    return "image/jpg";
+  }
+  if (strcmp(ext, "css") == 0) {
+    return "text/css";
+  }
+  if (strcmp(ext, "js") == 0) {
+    return "application/javascript";
+  }
+  if (strcmp(ext, "json") == 0) {
+    return "application/json";
+  }
+  if (strcmp(ext, "txt") == 0) {
+    return "text/plan";
+  }
+  if (strcmp(ext, "gif") == 0) {
+    return "image/gif";
+  }
+  if (strcmp(ext, "png") == 0) {
+    return "image/png";
+  }
+
+  return TIPO_MIME_PREDETERMINADO;
+}
---
+++ Mime.h
@@ -0,0 +1,6 @@
+#ifndef _MIME_H_
+#define _MIME_H_
+
+extern char* Obtener_tipo_mime(char* nombre_archivo);
+
+#endif //!_MIME_H_
