c9cfe6a6e9dde982ec4b1fd71b846ce2dcf0d3be
Panchis7u7 <smadrigal.rod@gmail.com>
2021-02-10 18:53:54 -0600

    GET api satisfactoria.

--- Api.c
+++ Api.c
@@ -7,30 +7,33 @@ void handleApi(int fd, char* api,  MYSQL* conn, int(*f)(int fd, char* cabeza, ch
         MYSQL_RES* res;
         MYSQL_ROW row;
         unsigned int num_fields;
-        char body[700];
+        char body[1200];
         int length = 0;
 
         mysql_query(conn, "SELECT * FROM Alumnos;");
         res = mysql_store_result(conn);
         num_fields = mysql_num_fields(res);
-        char* headers[num_fields];
+        MYSQL_FIELD* fields[num_fields];
 
         for(unsigned int i = 0; (field = mysql_fetch_field(res)); i++) {
-            headers[i] = field->name;
+            fields[i] = field;
         }
-        printf("number of fields: %d\n", num_fields);
         
         length += snprintf(body+length, sizeof(body), "[");
         while(row = mysql_fetch_row(res)){
             length += snprintf(body+length, sizeof(body), "{");
-            for(int i = 0; i < num_fields; i++){
-                length += snprintf(body+length, sizeof(body),"\"%s\": %s, ", headers[i], row[i]);
+            for(int i = 0; i < num_fields-1; i++){
+                if(IS_NUM(fields[i]->type))
+                    length += snprintf(body+length, sizeof(body),"\"%s\": %s, ", fields[i]->name, row[i]);
+                else
+                    length += snprintf(body+length, sizeof(body),"\"%s\": \"%s\", ", fields[i]->name, row[i]);
             }
             length += snprintf(body+length, sizeof(body), "}, ");
         }
         length += snprintf(body+length, sizeof(body), "]");
 
         (*f)(fd, "HTTP/1.1 200 OK", "application/json", body, length);
+        length = 0;
         mysql_free_result(res);
     }
 }
